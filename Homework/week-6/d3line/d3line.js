8248dae69f10961288098f7228ee68df20c3c753
PatrickJonk <patrick_jonk@hotmail.com>
2015-12-05 03:38:10 +0100

    warning!
    the map only runs from the week-6 file and not the d3map file

--- Homework/week-6/d3line/d3line.js
+++ Homework/week-6/d3line/d3line.js
@@ -1,10 +1,15 @@
+// Patrick Jonk
+// 10001336
+// d3line.js
+
 var data;
 
+// load data
 d3.json("data.json", function(error, json) {
 	if (error) return console.warn(error);
 	data = json;
 
-	console.log(new Date(data[data.length - 1][0]));
+	// some constants
 	width = 1000,
 	height = 500,
 	margin = {
@@ -13,19 +18,23 @@ d3.json("data.json", function(error, json) {
 		bottom: 20,
 		left: 50
 		}
-	var graph = d3.select("#linegraph");
-		range_x = d3.time.scale().range([margin.left, width - margin.right]).domain([new Date(data[0][0]), new Date(data[data.length - 1][0])]);
 
+	// select html element
+	var graph = d3.select("#linegraph");
+
+		// linear transformation functions
+		range_x = d3.time.scale().range([margin.left, width - margin.right]).domain([new Date(data[0][0]),
+		 new Date(data[data.length - 1][0])]);
 		range_y = d3.scale.linear().range([height - margin.top, margin.bottom]).domain([-5, 35]);
-	
+
+		// transformation from x - pixel to index
 		reverse_range_x = d3.scale.linear().range([0, 365]).domain([margin.left, width - margin.right]);
 
-		console.log(data.length - 1);
-
-		
+		// create axis
 		x_axis = d3.svg.axis().scale(range_x).tickSize(5).tickSubdivide(true);
 		y_axis = d3.svg.axis().scale(range_y).tickSize(5).orient("left");
 
+	// append axis to graph
 	graph.append('svg:g')
 	.attr('class', 'x axis')
 	.attr('transform', 'translate(0,' + (height - margin.bottom) + ')')
@@ -36,6 +45,7 @@ d3.json("data.json", function(error, json) {
 	.attr('transform', 'translate(' + margin.left + ', 0)')
 	.call(y_axis);
 
+	// plot graph
 	var line = d3.svg.line()
 		.x(function(d) {
 			return range_x(new Date(d[0]));
@@ -44,23 +54,18 @@ d3.json("data.json", function(error, json) {
 			return range_y(d[1]/10);
 		})
 
+	// append plot tot graph
 	graph.append('svg:path')
 		.attr('d', line(data))
 		.attr('stroke', 'blue')
 		.attr('stroke-width', 2)
 		.attr('fill', 'none');
 
-	var date_box = d3.select(".date").text('onzin');
+	// gets data info boxes
+	var date_box = d3.select(".date")
 	var temp_box = d3.select(".temperature")
 
-	// var g = graph.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
-	// var line = graph.append('line')
-	// 	.style("stroke", "black")  
-	//     .attr("x1", 100)     
-	//     .attr("y1", margin.top)    
-	//     .attr("x2", 100)    
-	//     .attr("y2", height - margin.bottom);
+	// make lines for crosshair
 	var vertical_line = graph.append('line')
 			.style("stroke", "black")
 			.style("opacity", 0)  
@@ -77,81 +82,79 @@ d3.json("data.json", function(error, json) {
 		    .attr("x2", width - margin.right)    
 		    .attr("y2", 0);
 
+	// define timer for tooltip delay
 	timer = null;
 
+	// if the mouse is moved the crosshair moves and the data shown after a delay
 	graph.on('mousemove', function(d) {
 
+		// tooltip is not shown if mouse moves
 		clearTimeout(timer);
-		
+
+		// gets the position of the mouse
 		position = d3.mouse(this);
 		x = position[0];
 		y = position[1];
 
+		// converts horizontal position into an index for looking up data
 		index = Math.round(reverse_range_x(x));
 
+		// update crosshair position
 		vertical_line.style("opacity", 1)
 			.attr("x1", x)
 			.attr("x2", x);
 
+		// data for tooltip and crosshair
 		temperature = data[Math.round(reverse_range_x(x))][1] / 10;
 		date = data[index][0];
 
+		// update crosshair position
 		horizontal_line.style("opacity", 1)
 			.attr("y1", range_y(temperature))
 			.attr("y2", range_y(temperature));
 
-		date_box.style('left', x + 'px').style('top', y + 'px');
+
+		//date_box.style('left', x + 'px').style('top', y + 'px');
 		date_box.text(date);
 
 		
 		temp_box.text(temperature);
 
+		// adjust div elements positions
 		if (x < width/2){
 			temp_box.style('left', (3 * width / 4) +'px');
-			temp_box.style('top', range_y(temperature) - 10 + 'px');
+			temp_box.style('top', range_y(temperature) - 12 + 'px');
 							
 		}
 		else if (x => width / 2){
 
 			temp_box.style('left', (width / 4) +'px');
-			temp_box.style('top', range_y(temperature) - 10 + 'px');
+			temp_box.style('top', range_y(temperature) - 12 + 'px');
 		}
 
 		if (range_y(temperature) < height / 2){	
-			date_box.style('left', (x - 52) + 'px');
+			date_box.style('left', (x - 40) + 'px');
 			date_box.style('top', (3 * height / 4) + 'px');							
 		}
 		// else if (range_y(temperature) => height / 2){
-		// 	date_box.style('left', x - 52 + 'px');
+		// 	date_box.style('left', (x - 52) + 'px');
 		// 	date_box.style('top', 10 + (height / 4) +'px');
-
-			
 		// }
 
-		date_box.style('border', 1 + 'px solid #000000');
-
+		// gives di elements an outline
+		date_box.style('border', 1 + 'px solid #000000')
 		temp_box.style('border', 1 + 'px solid #000000');
 
-	    // console.log(line.style("stroke"));
-
+		// hide div elements if mouse moves
 		date_box.style("visibility", 'hidden');
-
 		temp_box.style("visibility", 'hidden');
 	    
-		
+		// make tooltip visible after delay
 		timer = setTimeout(function(){ 
 			date_box.style("visibility", 'visible');
 			temp_box.style("visibility", 'visible');
-
-
 		}, 1000);
-    	console.log(temperature);
-    	console.log(data[index][0]);
   });
-		// .on('mouseout', function(d) {
-		// 	line.style("opacity", 0);
-
-		// })
 		
 
 });
